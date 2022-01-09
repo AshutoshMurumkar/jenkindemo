57abfa7b67893482b8a9afd8bb65ae653e244354
ashutosh <ashutosh@gmail.com>
2022-01-09 10:24:38 +0530

    Added java demo application files

---
+++ src/main/java/org/example1/Java_jenkin_in_docker/App.java
@@ -0,0 +1,22 @@
+package org.example1.Java_jenkin_in_docker;
+
+/**
+ * Hello world!
+ *
+ */
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(checkEvenOrOdd(23));
+    }
+
+	public static boolean checkEvenOrOdd(int i) {
+		// TODO Auto-generated method stub
+		return i %2 ==0;
+	}
+
+	
+}
